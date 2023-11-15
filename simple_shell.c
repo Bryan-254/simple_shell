d1f0b9a54a4daafaa0fc11dca72efbe9a2119aee
UserBryan <bkk@gmail.com>
2023-11-15 08:26:30 -0800

    Command File

--- simple_shell.c
+++ simple_shell.c
@@ -1,17 +1,18 @@
-#include "simple_shell.h"
+#include "shell.h"
 char *name;
+
 /**
- * main - print prompt, handle EOF, read file_stream
- * @argc: arg count (not needed)
- * @argv: argv for command
- * Return: Always 0.
+ * main - Function will print prompt, handle End of File & read file_stream
+ * @argc: This parameter is the arg count (not needed hence void attached)
+ * @argv: This parameter is the argv for command
+ * Return: zero
  */
 
 int main(int argc, char *argv[])
 {
-	char *s = NULL;
-	size_t buffer_size = 0;
-	ssize_t file_stream = 0;
+	char *input_buffer = NULL;
+	size_t sizeof_buffer = 0;
+	ssize_t getline_retval = 0;
 
 	(void) argc;
 	name = argv[0];
@@ -19,98 +20,120 @@ int main(int argc, char *argv[])
 	while (1)
 	{
 		if (isatty(STDIN_FILENO) == 1)
+		{
 			write(1, "$ ", 2);
-		file_stream = getline(&s, &buffer_size, stdin);
-		if (file_stream == -1)
+		}
+		getline_retval = getline(&input_buffer, &sizeof_buffer, stdin);
+		if (getline_retval == -1)
 		{
 			if (isatty(STDIN_FILENO) == 1)
+			{
 				write(1, "\n", 1);
+			}
 			break;
 		}
 
-		if (s[file_stream - 1]  == '\n')
-			s[file_stream - 1]  = '\0';
-		if (*s == '\0')
+		if (input_buffer[getline_retval - 1]  == '\n')
+		{
+			input_buffer[getline_retval - 1]  = '\0';
+		}
+		if (*input_buffer == '\0')
+		{
 			continue;
-		if (cmd_read(s, file_stream) == 2)
+		}
+		if (cmd_read(input_buffer, getline_retval) == 2)
+		{
 			break;
+		}
 	}
-	free(s);
-	s = NULL;
+	free(input_buffer);
+	input_buffer = NULL;
 	return (0);
 }
+
 /**
- * cmd_read - handles command line and tokenizes it
- *@s: string
- *@file_stream: getline input
+ * cmd_read - This function the handles command line and then tokenizes it
+ * @st: This parameter is the string
+ * @getline_retval: This parameter is the getline input
  * Return: 0
  */
-int cmd_read(char *s, size_t __attribute__((unused))file_stream)
-{
-	char *token = NULL;
-	char *cmd_arr[100];
-	int i;
 
-	if (_strcmp(s, "exit") == 0)
+int cmd_read(char *st, size_t __attribute__((unused))getline_retval)
+{
+	char *curr_arg = NULL;
+	char *cmd_array[100];
+	int x;
+
+	if (_strcmp(st, "exit") == 0)
+	{
 		return (2);
-	if (_strcmp(s, "env") == 0)
+	}
+	if (_strcmp(st, "env") == 0)
+	{
 		return (_printenv());
-	token = strtok(s, " "), i = 0;
+	}
+	curr_arg = strtok(st, " "), x = 0;
+
 	while (token)
 	{
-		cmd_arr[i++] = token;
-		token = strtok(NULL, " ");
+		cmd_array[x++] = token;
+		curr_arg = strtok(NULL, " ");
 	}
-	cmd_arr[i] = NULL;
-/* Return status code */
-	return (call_command(cmd_arr));
+	cmd_array[x] = NULL;
+	return (call_thecommand(cmd_array));
 }
+
 /**
- * print_not_found - prints when cmd is not found in path
- *
- * @cmd: a string provided by the stdin
+ * print_cmdnotfound - This function will print when cmd is not found in path
+ * @cmd: This function is a string provided by the stdin
+ * Return: nothing
  */
-void print_not_found(char *cmd)
+
+void print_cmdnotfound(char *cmd)
 {
 	write(2, name, _strlen(name));
 	write(2, ": 1: ", 5);
 	write(2, cmd, _strlen(cmd));
 	write(2, ": not found\n", 12);
 }
+
 /**
- * call_command - calls cmd, forks, execve
- *
- * @cmd_arr: a string provided by the stdin
+ * call_thecommand - This function will call cmd, forks, execve
+ * @cmd_array: This parameter is a string provided by the stdin
  * Return: 0
  */
-int call_command(char *cmd_arr[])
-{
-	char *exe_path_str = NULL;
-	char *cmd = NULL;
-	pid_t is_child;
-	int status;
 
-	cmd = cmd_arr[0];
-	exe_path_str = pathfinder(cmd);
-	if (exe_path_str == NULL)
+int call_thecommand(char *cmd_array[])
+{
+	char *str_of_exepath = NULL;
+	char *cmd = NULL;
+	pid_t child_pid;
+	int status_exit;
+
+	cmd = cmd_array[0];
+	str_of_exepath = pathfinder(cmd);
+
+	if (str_of_exepath == NULL)
 	{
-		print_not_found(cmd);
+		print_cmdnotfound(cmd);
 		return (3);
 	}
-	is_child = fork();
-	if (is_child < 0)
+	child_pid = fork();
+	if (child_pid < 0)
 	{
 		perror("Error:");
 		return (-1);
 	}
 	if (is_child > 0)
-		wait(&status);
-	else if (is_child == 0)
 	{
-		(execve(exe_path_str, cmd_arr, environ));
+		wait(&status_exit);
+	}
+	else if (child_pid == 0)
+	{
+		(execve(str_of_exepath, cmd_array, environ));
 		perror("Error:");
 		exit(1);
 	}
-	free(exe_path_str);
+	free(str_of_exepath);
 	return (0);
 }
